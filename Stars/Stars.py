c9c66b9f76c5060a02332572fee01373dcaf343f
mahclark <mc2124@cam.ac.uk>
2021-07-13 16:43:26 +0100

    rewrote stars project

--- Stars/Stars.py
+++ Stars/Stars.py
@@ -1,131 +1,188 @@
+import csv
+import parse
 import pygame
 import time
 import os
-from functions import read
+from dataclasses import dataclass, replace
 from math import radians, sin, cos, tan, sqrt, pi
+from typing import NamedTuple
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
-pygame.init()
+@dataclass
+class Vec3:
+    x : float = 0
+    y : float = 0
+    z : float = 0
 
-xSize, ySize = 1600, 800
-screen = pygame.display.set_mode((xSize, ySize))
-pygame.display.set_caption("Pygame Template")
+    def __add__(self, vec):
+        return Vec3(
+            self.x + vec.x,
+            self.y + vec.y,
+            self.z + vec.z
+        )
 
-depth = 1000 #distance between thoertical screen and eye (changes fov)
+    def __sub__(self, vec):
+        return self + -vec
 
-ry = -30 #rotation about y axis
-rx = 0 #rotation about x axis
+    def __neg__(self):
+        return Vec3(
+            -self.x,
+            -self.y,
+            -self.z
+        )
 
-class c: x, y, z = 50,80,-50 #camera coordinates
+    def xy(self):
+        return (
+            int(self.x),
+            int(self.y)
+        )
 
-def get_2d(x, y, z): #Algorithm to convert 3D coordinates into 2D screen coordinates
-    x -= c.x
-    y -= c.y
-    z -= c.z
 
-    oldx = x
-    oldy = y
-    
-    #turning camera
-    x = x*cos(radians(rx)) - z*sin(radians(rx))
-    z = z*cos(radians(rx)) + oldx*sin(radians(rx))
+class Star(Vec3):
 
-    y = y*cos(radians(ry)) - z*sin(radians(ry))
-    z = z*cos(radians(ry)) + oldy*sin(radians(ry))
+    def __init__(self, ra, dec, dist):
+        """
+        Takes the right ascension, declination and distance from Earth
+        and calculates the 3D coordinates of the star
+        """
+        hth = ra*pi/12
+        vth = dec*pi/180
 
-    z -= depth
+        super().__init__(
+            dist*cos(vth)*sin(hth),
+            dist*sin(vth),
+            dist*cos(vth)*cos(hth)
+        )
 
-    class e: x, y, z = 0,0,-depth #eye coordinates
-    
-    try: x = x + (0 - z)*(x - e.x)/(z - e.z)
-    except: x = c.x
-    try: y = y + (0 - z)*(y - e.y)/(z - e.z)
-    except: y = c.y
 
-    x += xSize/2
-    y = ySize/2 - y
+if __name__ == "__main__":
 
-    if z <= e.z: return "error"
-    return int(x), int(y)
+    pygame.init()
 
-stars_txt = read("\Stars.txt").split("\n")
+    x_size, y_size = 1600, 800
+    screen = pygame.display.set_mode((x_size, y_size))
+    pygame.display.set_caption("Stars")
 
-stars = {}
-stars["num"] = 0
+    depth = 500 # fov controller (higher decreases fov)
 
-for txt in stars_txt:
-    stars["name{0}".format(stars["num"])] = txt[:12]
-    stars["mag{0}".format(stars["num"])] = float(txt[35:39])
-    stars["ra{0}".format(stars["num"])] = int(txt[40:42]) + int(txt[43:46])/60 + float(txt[49:54])/3600
-    stars["dec{0}".format(stars["num"])] = float(txt[56:59]) + float(txt[56] + txt[61:63])/60 + float(txt[56] + txt[65:69])/3600
-    stars["dist{0}".format(stars["num"])] = float(txt[71:])
-    stars["num"] += 1
-    
+    rx = -pi # rotation about x axis
+    ry = pi/3 # rotation about y axis
 
-#print(stars["name0"],stars["mag0"],stars["ra0"],stars["dec0"],stars["dist0"])
-#print(stars["name4"],stars["mag4"],stars["ra4"],stars["dec4"],stars["dist4"])
+    camera = Vec3()
 
-#----------------------Main Loop----------------------#
+    def draw(surface, color, pos, camera, rx, ry):
+        """
+        Converts 3D coordinates to 2D coordinates
+        Renders as dot on the surface provided
+        """
 
-clock = pygame.time.Clock()
-frameCount = 0
-done = False
-while not done:
-    frameCount += 1
-    keys = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # convert world coordinates to camera coordinates
+        pos -= camera
+
+        # rotate coordinates around camera
+        original = replace(pos) # deep copies dataclass
+        
+        pos.x = pos.x*cos(rx) - pos.z*sin(rx)
+        pos.z = pos.z*cos(rx) + original.x*sin(rx)
+
+        pos.y = pos.y*cos(ry) - pos.z*sin(ry)
+        pos.z = pos.z*cos(ry) + original.y*sin(ry)
+
+        # don't draw anything behind us
+        if pos.z <= 1: # choose 1 not zero so the next part doesn't explode
+            return
+
+        # this part is the 3D to 2D magic
+        pos.x *= depth/pos.z
+        pos.y *= depth/pos.z
+
+        # adjust so (0,0) is in the middle, not the corner
+        pos.x += x_size/2
+        pos.y = y_size/2 - pos.y
+
+        pygame.draw.circle(surface, color, pos.xy(), 1)
+
+    # Read and parse the star data
+    stars = []
+
+    with open("Stars.txt", "rb") as stars_txt:
+
+        for line in stars_txt.read().decode(encoding='UTF-8').split("\n"):
+            _, _, ra_splt, dec_splt, dist_splt = line.split(",")
+            
+            h, m, s = parse.parse("{:d}h {:d}m {:f}s", ra_splt)
+            ra = h + m/60 + s/3600
+
+            sign, h, m, s = parse.parse("{}{:d}° {:d}′ {:f}″", dec_splt)
+            dec = float(sign + "1")*(h + m/60 + s/3600)
+
+            dist = float(dist_splt)
+
+            stars.append(Star(ra, dec, dist))
+
+    #----------------------Main Loop----------------------#
+    clock = pygame.time.Clock()
+    done = False
+    while not done:
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        if keys[pygame.K_ESCAPE]:
+            pygame.mouse.set_visible(True)
+            pygame.event.set_grab(False)
+        else:
+            delta_x, delta_y = pygame.mouse.get_rel()
+            pygame.mouse.set_visible(False)
+            pygame.event.set_grab(True)
+
+            rx += radians(delta_x/30)
+            ry -= radians(delta_y/30)
+
+            ry = min(ry, pi/2)
+            ry = max(ry, -pi/2)
+
+        if keys[pygame.K_w]:
+            camera.x += sin(rx)
+            camera.z += cos(rx)
+
+        if keys[pygame.K_a]:
+            camera.x += sin(3*pi/2 - rx)
+            camera.z -= cos(3*pi/2 - rx)
+
+        if keys[pygame.K_s]:
+            camera.x -= sin(pi - rx)
+            camera.z += cos(pi - rx)
+
+        if keys[pygame.K_d]:
+            camera.x += sin(pi/2 - rx)
+            camera.z -= cos(pi/2 - rx)
+
+        if keys[pygame.K_SPACE]:
+            camera.y += 1
+
+        if keys[pygame.K_LCTRL]:
+            camera.y -= 1
+
+        if keys[pygame.K_h]:
+            camera.x = 0
+            camera.z = 0
+            camera.y = 0
+
+        if keys[pygame.K_q]:
             done = True
 
-    if keys[pygame.K_ESCAPE]:
-        pygame.mouse.set_visible(True)
-        pygame.event.set_grab(False)
-    else:
-        delta_x, delta_y = pygame.mouse.get_rel()
-        pygame.mouse.set_visible(False)
-        pygame.event.set_grab(True)
-        rx += delta_x/30
-        ry -= delta_y/30
-        if ry > 90: ry = 90
-        if ry < -90: ry = -90
-    if keys[pygame.K_w]:
-        c.x += sin(radians(rx))
-        c.z += cos(radians(rx))
-    if keys[pygame.K_a]:
-        c.x += sin(radians(270 - rx))
-        c.z += -cos(radians(270 - rx))
-    if keys[pygame.K_s]:
-        c.x += -sin(radians(180 - rx))
-        c.z += cos(radians(180 - rx))
-    if keys[pygame.K_d]:
-        c.x += sin(radians(90 - rx))
-        c.z += -cos(radians(90 - rx))
-    if keys[pygame.K_SPACE]:
-        c.y += 1
-    if keys[pygame.K_LCTRL]:
-        c.y -= 1
-    if keys[pygame.K_p]:
-        c.x = 0
-        c.z = 0
-        c.y = 0
+        screen.fill([0,0,0])
 
-    screen.fill([0,0,0])
+        earth = Vec3()
 
-    try: pygame.draw.circle(screen, [0,255,0], (get_2d(0,0,0)),1)
-    except: pass
+        draw(screen, [0,255,0], earth, camera, rx, ry)
 
-    for star in range(stars["num"]):
-        hth = stars["ra{0}".format(star)]*pi/12
-        vth = stars["dec{0}".format(star)]*pi/180
-        d = stars["dist{0}".format(star)]
-        try: pygame.draw.circle(screen, [255,255,255], (get_2d(d*cos(vth)*sin(hth),d*sin(vth),d*cos(vth)*cos(hth))),0)#int(10/stars["mag{0}".format(star)]))
-        except: pass
-        #label = pygame.font.SysFont("monospace", 8).render(stars["name{0}".format(star)], 1, [255,255,255])
-        #try: screen.blit(label, (get_2d(d*sin(hth),d*tan(vth)-1,d*cos(hth))))
-        #except: pass
+        for star in stars:
+            draw(screen, [255,255,255], star, camera, rx, ry)
 
-    pygame.display.flip()
-    clock.tick(60)
+        pygame.display.flip()
+        clock.tick(60)
 
-pygame.quit()
+    pygame.quit()
